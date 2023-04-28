5ce80d02babd66160dc66e802013a7956dec71a9
Jong1co <narira0922@gmail.com>
2023-04-28 23:56:21 +0900

    [#12981] 영어 끝말잇기 리팩터링

--- programmers/12981.js
+++ programmers/12981.js
@@ -1,4 +1,30 @@
+/**
+ * forEach를 통해 무조건 끝까지 루프를 돌 필요가 없다고 판단해서 for..of를 이용해 루프를 돌리고, 탈락자가 나올 시 바로 정답을 리턴하도록 변경
+ */
 function solution(n, words) {
+  let hash = {};
+  let i = 0;
+
+  for (let word of words) {
+    if (hash[word]) return [(i % n) + 1, Math.floor(i / n) + 1];
+
+    i++;
+
+    if (i < words.length && word[word.length - 1] !== words[i][0]) {
+      return [(i % n) + 1, Math.floor(i / n) + 1];
+    }
+
+    hash[word] = true;
+  }
+
+  return [0, 0];
+}
+
+/**
+ * 기존 코드
+ */
+
+function solution2(n, words) {
   let hash = {};
   let answer = [];
 
