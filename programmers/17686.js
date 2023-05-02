75f3e5ff2597962b58360556ef84e423f835de65
Jong1co <narira0922@gmail.com>
2023-05-02 13:35:58 +0900

    [#17686] 실패한 로직 추가

--- programmers/17686.js
+++ programmers/17686.js
@@ -17,3 +17,34 @@ function solution(files) {
     })
     .map((file) => file[2]);
 }
+
+// 원래 접근법
+// 실패 이유 : split을 사용하면 tail에 같은 숫자가 반복될 시에 함께 삭제되기 때문에 split을 사용해선 안 됨
+function solution2(files) {
+  return files
+    .map((file) => {
+      const number = file.match(/\d{1,5}/g)[0];
+      const array = file.split(number);
+      array.splice(1, 0, number);
+
+      return array;
+    })
+    .sort((a, b) => {
+      if (a[0].toLowerCase() !== b[0].toLowerCase()) {
+        let i = 0;
+        while (a[0][i] === b[0][i]) {
+          i++;
+        }
+        if (!a[0][i] || !b[0][i]) {
+          return a[0].length - b[0].length;
+        }
+        return (
+          a[0][i].toLowerCase().charCodeAt() -
+          b[0][i].toLowerCase().charCodeAt()
+        );
+      }
+      if (Number(a[1]) !== Number(b[1])) return Number(a[1]) - Number(b[1]);
+      return 0;
+    })
+    .map((file) => file.join(""));
+}
