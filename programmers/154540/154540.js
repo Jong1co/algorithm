75be5d6900b0931072b20418afde46b62912f6c6
Jong1co <narira0922@gmail.com>
2023-05-22 09:40:43 +0900

    무인도 여행 성능 향상
    매번 while문을 돌 때마다 전체 루프를 돌아야 하기 때문에 성능 좋지 않음
    
    - for문을 한 번 돌면서 검사해 성능 향상

--- programmers/154540/154540.js
+++ programmers/154540/154540.js
@@ -1,42 +1,28 @@
 //솔루션을 함수를 복붙해주세요
-
-// 48분
+//48분
 function solution(maps) {
-  let reg = /\d/;
   let answer = [];
-  let startPoint = [];
-  let queue = [];
   maps = maps.map((map) => map.split(''));
 
-  while (
-    maps
-      .map((map) => map.join(''))
-      .join('')
-      .match(reg)
-  ) {
-    for (let y = 0; y < maps.length; y++) {
-      const x = maps[y].findIndex((map) => map.match(reg));
-      if (x !== -1) {
-        startPoint = [y, x];
-        break;
+  for (let y = 0; y < maps.length; y++) {
+    for (let x = 0; x < maps[0].length; x++) {
+      if (maps[y][x] === 'X') continue;
+      let count = 0;
+      let stack = [[y, x]];
+
+      while (stack.length !== 0) {
+        const [y, x] = stack.pop();
+        if (maps[y][x] === 'X') continue;
+        count += Number(maps[y][x]);
+        maps[y][x] = 'X';
+
+        if (maps[y][x - 1] && maps[y][x - 1] !== 'X') stack.push([y, x - 1]);
+        if (maps[y][x + 1] && maps[y][x + 1] !== 'X') stack.push([y, x + 1]);
+        if (maps[y - 1] && maps[y - 1][x] !== 'X') stack.push([y - 1, x]);
+        if (maps[y + 1] && maps[y + 1][x] !== 'X') stack.push([y + 1, x]);
       }
+      answer.push(count);
     }
-    queue.push(startPoint);
-
-    let count = 0;
-    while (queue.length !== 0) {
-      const [y, x] = queue.pop();
-      if (maps[y][x] === 'X' || maps[y] === undefined || maps[y][x] === undefined) continue;
-      count += Number(maps[y][x]);
-      maps[y][x] = 'X';
-
-      if (maps[y][x - 1] && maps[y][x - 1] !== 'X') queue.push([y, x - 1]);
-      if (maps[y][x + 1] && maps[y][x + 1] !== 'X') queue.push([y, x + 1]);
-      if (maps[y - 1] && maps[y - 1][x] !== 'X') queue.push([y - 1, x]);
-      if (maps[y + 1] && maps[y + 1][x] !== 'X') queue.push([y + 1, x]);
-    }
-    answer.push(count);
   }
-
   return answer.length === 0 ? [-1] : answer.sort((a, b) => a - b);
 }
