bdf9ca8d2a1393887204c83ea937cef59d1d5e05
Jong1co <narira0922@gmail.com>
2023-06-02 21:22:22 +0900

    [programmers] 두 큐 합 같게 만들기 리팩터링

--- programmers/118667/118667-1.js
+++ programmers/118667/118667-1.js
@@ -1,4 +1,3 @@
-//솔루션을 함수를 복붙해주세요
 function solution(queue1, queue2) {
   let q1_idx = 0;
   let q2_idx = 0;
@@ -9,8 +8,8 @@ function solution(queue1, queue2) {
   const sum = q1 + q2;
   if (sum % 2 === 1) return -1;
 
-  while (q1_idx < queue1.length && q2_idx < queue2.length && q1 !== sum / 2 && q2 !== sum / 2) {
-    if (q1_idx > queue.length * 3 || q2_idx > queue.length * 3) return -1;
+  while (q1 !== sum / 2) {
+    if (q1_idx > queue.length) return -1;
     if (q1 < sum / 2) {
       q1 += queue2[q2_idx];
       queue1.push(queue2[q2_idx]);
