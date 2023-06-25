33f55fb9ba4e1effa0b1b651ec91a1750ae0b245
Jong1co <narira0922@gmail.com>
2023-06-25 13:01:36 +0900

    [programmers] 줄서는 방법 리팩터링

--- programmers/12936/12936-2.js
+++ programmers/12936/12936-2.js
@@ -1,20 +1,17 @@
 //솔루션을 함수를 복붙해주세요
 function solution(n, k) {
   const people = Array.from({ length: n }, (v, i) => i + 1);
-  let idx = 0;
   const answer = [];
-
   let factorial = people.reduce((a, b) => a * b, 1);
   k -= 1;
-  while (k > 0) {
-    factorial = factorial / (n - idx);
 
+  while (k > 0) {
+    factorial = factorial / n;
     const index = Math.floor(k / factorial);
     answer.push(people[index]);
     people.splice(index, 1);
-
     k %= factorial;
-    idx++;
+    n--;
   }
 
   return answer.concat(people);
