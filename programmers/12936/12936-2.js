601c4ac3ef0fbf66711f702b04b24c5537f464cf
Jong1co <narira0922@gmail.com>
2023-06-25 12:58:03 +0900

    [programmers] 누락된 풀이 추가

--- programmers/12936/12936-2.js
+++ programmers/12936/12936-2.js
@@ -1,1 +1,21 @@
 //솔루션을 함수를 복붙해주세요
+function solution(n, k) {
+  const people = Array.from({ length: n }, (v, i) => i + 1);
+  let idx = 0;
+  const answer = [];
+
+  let factorial = people.reduce((a, b) => a * b, 1);
+  k -= 1;
+  while (k > 0) {
+    factorial = factorial / (n - idx);
+
+    const index = Math.floor(k / factorial);
+    answer.push(people[index]);
+    people.splice(index, 1);
+
+    k %= factorial;
+    idx++;
+  }
+
+  return answer.concat(people);
+}
